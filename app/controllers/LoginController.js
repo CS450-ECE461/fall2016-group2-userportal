5bb7a9998317367e2f6f429770fe47b81a8f8b07
Rob <robjansen15@gmail.com>
2016-11-05 15:48:13 -0400

    save

--- app/controllers/LoginController.js
+++ app/controllers/LoginController.js
@@ -10,44 +10,49 @@ function LoginController () {
 blueprint.controller (LoginController);
 
 LoginController.prototype.echoName = function () {
-  var self = this;
+    var self = this;
 
-  return function (req, res) {
+    return function (req, res) {
 
-    //preliminary checks before sending to the api server
-    if(req.body.username.length < 5){
-      return res.render ('login.pug', {message: "Username not valid"});
-    }
+        //preliminary checks
+        if(req.body.name.length < 5){
+            return res.render ('login.pug', {message: "Username not valid"});
+        }
 
-    if(req.body.password.length < 5){
-       return res.render ('login.pug', {message: "Password not valid"});
-    }
-
-
+        if(req.body.password.length < 5){
+            return res.render ('login.pug', {message: "Password not valid"});
+        }
 
 
-      var userData = {
-          username: req.body.username,
-          password: req.body.password
-      }
+        var userData = {
+            "user" : {
+                "username"  : "djpeck",
+                "password"  : "test",
+                "email"     : "djpeck@iupui.edu",
+                "job_title" : "Software Engineer"
+            }
+        }
 
-      //api server requests
-      request
-          .post('localhost:5000/login')
-          .send(userData)
-          //.set('Authorization', 'bearer ' + access_token)
-          .end(function (err, res) {
-              if (err) {
-                  console.log(err);
-              } else {
-                  console.log (res.token);
-                  return res.render ('loggedin.pug', {name: req.body.name});
-              }
-          });
+        //api server requests
+        request
+            .post('localhost:5000/dev/users')
+            .send(userData)
+            //.set('Authorization', 'bearer ' + access_token)
+            .end(function (err, res) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log (res.token);
+                    return res.render ('loggedin.pug', {});
+                }
+            });
 
-
-    return res.render ('login.pug', {message: "Error logging in." + req.body.user});
-  };
+        return res.render ('login.pug', {message: "Error logging in."});
+    };
 };
 
 module.exports = exports = LoginController;
+
+
+
+
