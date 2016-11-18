1e17cabadd5619bf227b71bc436780f615f9137b
Rob <robjansen15@gmail.com>
2016-11-18 00:05:33 -0500

    added lincoln's page into development after pull request.

--- app/controllers/UserStrategyController.js
+++ app/controllers/UserStrategyController.js
@@ -8,11 +8,68 @@ function UserStrategyController () {
 
 blueprint.controller (UserStrategyController);
 
+
+///
+/// This method checks the user session before proceeding
+///
+UserStrategyController.prototype.dashboardInit = function () {
+    return function (req, res) {
+        return res.render ('dashboard.pug', {message: 'Welcome to the dashboard!'});
+    };
+};
+
+
+///
+/// Logs the user from passport and the admin server
+///
 UserStrategyController.prototype.logout = function () {
     return function (req, res) {
         req.logout ();
         return res.redirect ('/login');
     };
 };
+
+
+///
+/// This supplies the user with the default view
+///
+UserStrategyController.prototype.home = function () {
+    return function (req, res) {
+        return res.render ('dashboard.pug', {message: 'Main view'});
+    };
+};
+
+
+///
+/// This supplies the user with the notification view
+///
+UserStrategyController.prototype.notificationsTab = function () {
+    return function (req, res) {
+        return res.render ('dashboard.pug', {message: 'Notifications view'});
+    };
+};
+
+
+///
+/// This supplies the user with the message generation view (compose)
+///
+UserStrategyController.prototype.composeTab = function () {
+    return function (req, res) {
+        return res.render ('dashboard.pug', {message: 'Compose view'});
+    };
+};
+
+
+///
+/// This supplies the user with the contacts view
+///
+UserStrategyController.prototype.contactsTab = function () {
+    return function (req, res) {
+        return res.render ('dashboard.pug', {message: 'Contacts view'});
+    };
+};
+
+
+
 
 module.exports = exports = UserStrategyController;
--- app/routers/UserStrategyRouter.js
+++ app/routers/UserStrategyRouter.js
@@ -4,13 +4,38 @@ var passport = require ('passport')
 module.exports = exports = {
     '/dashboard' : {
         get  : { view   : 'dashboard.pug'},
-        post : { action : 'UserStrategyController@logout'},
-        //use : isLoggedIn
+        post : { action : 'UserStrategyController@dashboardInit' }
+    },
+
+
+    '/dashboard/home' : {
+        get  : { view   : 'dashboard.pug' },
+        post : { action : 'UserStrategyController@home' }
+    },
+
+
+    '/dashboard/logout' : {
+        get  : { view   : 'dashboard.pug' },
+        post : { action : 'UserStrategyController@logout' }
+    },
+
+
+    '/dashboard/notificationsTab' : {
+        get  : { view   : 'dashboard.pug' },
+        post : { action : 'UserStrategyController@notificationsTab' }
+    },
+
+
+    '/dashboard/composeTab' : {
+        get  : { view   : 'dashboard.pug' },
+        post : { action : 'UserStrategyController@composeTab' }
+    },
+
+
+    '/dashboard/contactsTab' : {
+        get  : { view   : 'dashboard.pug' },
+        post : { action : 'UserStrategyController@contactsTab' }
     }
+
 };
 
-function isLoggedIn (req,res,next) {
-    /*if (req.isAuthenticated ())
-     return next ();
-     res.redirect ('/login');*/
-}
