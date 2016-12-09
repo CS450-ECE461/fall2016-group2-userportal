1aac4d77e7bf9a1870618665e3f4393b936d23d4
Danny Peck <danieljpeck93@gmail.com>
2016-12-09 14:56:45 -0500

    removed the close routes from the router

--- app/routers/UserStrategyRouter.js
+++ app/routers/UserStrategyRouter.js
@@ -29,12 +29,6 @@ module.exports = exports = {
         post : { action : 'UserStrategyController@compose' }
     },
 
-    '/dashboard/compose/close' : {
-        use: isLoggedIn,
-        get  : { view   : 'dashboard.pug' },
-        post : { action : 'UserStrategyController@composeClose' }
-    },
-
     '/dashboard/compose/send' : {
         use: isLoggedIn,
         get  : { view   : 'dashboard.pug' },
@@ -45,12 +39,5 @@ module.exports = exports = {
         use: isLoggedIn,
         get  : { view   : 'dashboard.pug' },
         post : { action : 'UserStrategyController@contacts' }
-    },
-
-    '/dashboard/contacts/close' : {
-        use: isLoggedIn,
-        get  : { view   : 'dashboard.pug' },
-        post : { action : 'UserStrategyController@contactsClose'}
     }
-
 };
