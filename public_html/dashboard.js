7df6b4cd375070b86e27fa6b53e5fad6db08b8b0
Danny Peck <danieljpeck93@gmail.com>
2016-12-09 22:35:22 -0500

    created Event Listeners to handle dashboard logic using JQuery and Ajax

---
+++ public_html/dashboard.js
@@ -0,0 +1,79 @@
+// registers all event listeners and logic for the user portal dashboard
+$(document).ready(function () {
+  // initialize the dashboard with none of the tabs showing
+  $('#contactsTab').css ('display', 'none');
+  $('#composeTab').css ('display', 'none');
+  $('#messagesTab').css ('display', 'none');
+
+  // Event Listener for Home NavBar Button
+  $('#home').on('click', function () {
+    $('.tab').hide();
+    $('.table').empty();
+  });
+
+  // Event Listener for Message NavBar Button
+  $('#messages').on('click', function () {
+    // Hides all tabs that are not the message tab
+    $('.tab:not(#messagesTab)').hide();
+
+    // Empties all of the tables that are not the messages table
+    $('.table:not(#messagesTable)').empty();
+
+    // if the message tab is not currently displayed fetch the data and display it
+    if ($('#messagesTab').css('display') == 'none') {
+      $.getJSON("/dashboard/messages", function (data) {
+        $("#messagesTable").append("<tr> <th> Sender </th> <th> Receiver </th> <th> Content </th> </tr>");
+        $.each(data, function (index, message) {
+          $("#messagesTable").append("<tr id='" + index + "'><td>" + message.sender_email + "</td><td>" + message.receiver_email + "</td><td>" + message.content + "</td></tr>");
+        });
+      });
+
+      $('#messagesTab').show();
+    }
+  });
+
+  // Event Listener for Compose Message NavBar Button
+  $('#compose').on('click', function () {
+    $('.tab:not(#composeTab)').hide();
+    $('.table').empty();
+    $('#composeTab').show();
+  });
+
+  // Event Listener for the close compose message tab
+  $('#closeComposeButton').on('click', function () {
+    $('#composeTab').hide();
+  });
+
+  // Event Listener for send message button
+  $('#send').on ('click', function () {
+    $.post ('/dashboard/compose/send', {}, function (message) {
+      console.log (message);
+      $('#closeComposeButton').trigger('click');
+    })
+    .fail(function (err) {
+      console.log (err);
+    });
+  });
+
+  // Event Listener for Contacts NavBar button
+  $('.contacts').on('click', function () {
+    $('.tab:not(#contactsTab)').hide();
+    $('.table:not(#contactsTable)').empty();
+
+    if ($('#contactsTab').css('display') == 'none') {
+      $.getJSON("/dashboard/contacts", function (data) {
+        $("#contactsTable").append("<tr> <th> UserName </th> <th> Email </th> <th> Job Title </th> </tr>");
+        $.each(data, function (index, user) {
+          $("#contactsTable").append("<tr id='" + index + "'><td>" + user.username + "</td><td>" + user.email + "</td><td>" + user.job_title + "</td></tr>");
+        });
+      });
+
+      $('#contactsTab').show();
+    }
+  });
+
+  // Event Listener for the close contacts tab
+  $('#closeContactsButton').on ('click', function () {
+    $('#contactsTab').hide();
+  });
+});
