2413dbe79a6902fee9a4b0bfa5370317da1b9b74
Lincoln <lrthayer@iupui.edu>
2016-12-10 21:31:15 -0500

    attempting to add hometab get userinfo
    just a save, currently the request doesn't print in the console until logout

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -23,7 +23,7 @@ DashboardController.prototype.init = function () {
 ///
 DashboardController.prototype.messages = function () {
     return function (req, res) {
-
+		console.log("!!!!");
         var token = req.user;
 
         var route = '/v1/messages';
@@ -118,8 +118,36 @@ DashboardController.prototype.contacts = function () {
                  }
              } else {
                var contacts = resp.body;
-
+			   console.log("test");
                return res.json (contacts);
+            }
+         });
+    };
+};
+
+DashboardController.prototype.userInfo = function () {
+    return function (req, res) {
+        var token = req.user;
+
+        var route = '/v1/users/:userid';
+        if (process.env.NODE_ENV == 'test') {
+            route = '/mock/userInfo';
+        }
+
+        request
+         .get (blueprint.app.configs.apiserver.baseuri + route)
+         .set ('Authorization', 'bearer ' + token)
+         .end (function (err, resp) {
+             if (err) {
+                 if (err.status == '400') {
+                   console.log('we got a 400');
+                   //return done (null,false,{message: "Error Sending Message"});
+                 }
+             } else {
+               var userInfo = resp.body;
+
+               return res.json (userInfo);
+               console.log("test");
             }
          });
     };
